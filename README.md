8e50a95bd55c39beac3b084d7b5a189f79740215
Rareș Ciubotaru <raresciubotaru@Raress-MacBook-Pro-2.local>
2024-09-17 16:23:11 +0300

    LSEG-03: update backend services

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # lseg-challenge
 A challange by LSEG
+
+[!NOTE]
+Setup of the project
+
+## BACKEND
+Use the next commands, in order in the terminal:
+
+cd backend --> go into backend directory
+npm install --> install dependecies & npm packages
+npm run dev --> this will run the server on http://localhost:3000 
+
+## FRONTEND
+
+cd frontend --> go into backend directory
+npm install --> install dependecies & npm packages
+npm run dev --> this will run the client on http://localhost:5173
+
+[!NOTE]
+General Info
+
+SERVER: uses a simple MVC architecture using express + typescript
+CLEINT: uses VITE + typescript + SCSS && React components follow a pattern of hooks to separate the business logic from the UI part
+
+P.S. The style of the client is super bad, also there is nothing optimized on React side, also regarding I don't think that I managed
+to take into consideration all errors that can happen.
--- backend/src/controllers/exchanges.controllers.ts
+++ backend/src/controllers/exchanges.controllers.ts
@@ -1,14 +1,25 @@
-import {Request, Response} from 'express';
-import {exchangeService} from '../services/index';
+import { Request, Response } from 'express';
+import { exchangeService } from '../services/index';
 import httpStatus from 'http-status';
 
 import catchAsync from '../utils/catchAsync';
 
-const fetchStockExhanges = catchAsync(async (req: Request, res: Response) => { 
-  const exchanges = await exchangeService.fetchStockExhanges();
-  res.status(httpStatus.OK).send(exchanges);
+const fetchStockExhanges = catchAsync(async (req: Request, res: Response) => {
+	const exchanges = await exchangeService.fetchStockExhanges();
+	res.status(httpStatus.OK).send(exchanges);
 });
 
-export {
-  fetchStockExhanges
-}
+const fetchStocks = catchAsync(async (req: Request, res: Response) => {
+	const exchangeCode = req.params.exchangeCode;
+	const exchanges = await exchangeService.fetchTopStocksOfExchange(exchangeCode);
+	res.status(httpStatus.OK).send(exchanges);
+});
+
+const fetchStockDetails = catchAsync(async (req: Request, res: Response) => {
+	const exchangeCode = req.params.exchangeCode;
+	const stockCode = req.params.stockCode;
+	const exchanges = await exchangeService.fetchStockDetails(exchangeCode, stockCode);
+	res.status(httpStatus.OK).send(exchanges);
+});
+
+export { fetchStockExhanges, fetchStocks, fetchStockDetails };
